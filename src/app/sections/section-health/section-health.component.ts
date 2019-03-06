43edb56d5a5653e73f58174ed32441a1ac4479f9
gAltelino <galtelino@gmail.com>
2019-03-06 01:15:27 -0300

    dormir

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-mail', isOnline: true},
+  {id: 3, name: 'dev-what', isOnline: false},
+  {id: 5, name: 'dev-hack', isOnline: false}
+];
 
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.css']
 })
+
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit() {
   }
