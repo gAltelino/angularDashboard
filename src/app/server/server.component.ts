aa0a8e7c295f52f64e94d09f557fe4d3a8cee883
gAltelino <galtelino@gmail.com>
2019-03-07 21:53:32 -0300

    server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,13 +10,31 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
   }
 
-  toggleStatus() {
-    console.log(this.serverInput.name);
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStatus(!onlineStatus);
   }
 
 }
